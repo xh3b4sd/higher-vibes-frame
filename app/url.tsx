aab7d2414063093f4e3b274effc317d577d56dcf
xh3b4sd <xh3b4sd@gmail.com>
2024-04-09 11:52:18 +0200

    fix vercel url

--- app/url.tsx
+++ app/url.tsx
@@ -2,7 +2,7 @@ export const newUrl = (pat: string): URL => {
   return new URL(
     pat,
     process.env.NEXT_PUBLIC_VERCEL_URL
-      ? `https://{process.env.NEXT_PUBLIC_VERCEL_URL}`
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
       : "http://localhost:3000"
   )
 }
