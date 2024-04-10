af0f8f9b4897fd318edadc99395bf6c26904d252
xh3b4sd <xh3b4sd@gmail.com>
2024-04-10 11:52:47 +0200

    fix routes (#8)

--- app/api/frames/submit/route.tsx
+++ app/api/frames/submit/route.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/jsx-key */
-import { frames } from "../../frames";
-import { verifyString } from "../../../utils/verify";
-import { invalidResponse } from "../../frames/response/invalid";
-import { finishResponse } from "../../frames/response/finish";
+import { frames } from "../../../frames";
+import { verifyString } from "../../../../utils/verify";
+import { invalidResponse } from "../../../frames/response/invalid";
+import { finishResponse } from "../../../frames/response/finish";
 import createRecord from "@/utils/airtable";
 
 // @ts-ignore
